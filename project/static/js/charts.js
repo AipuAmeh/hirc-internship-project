b9843af664b0885b0b414af57902d9620bcd851f
AipuAmeh <aipuameh.16@gmail.com>
2025-05-31 12:01:18 -0700

    charts now visible upon clicking separate buttons

--- project/static/js/charts.js
+++ project/static/js/charts.js
@@ -1,12 +1,60 @@
-const getCharts = (event) => {
-  const chart_type = event.target.value;
-      try {
-      fetch(`/api/data?chart_type=${chart_type}`)
-      .then((res) => res.json())
-      .then((chartData) => console.log(chartData))
-      } catch (error) {
-          console.log(error)
-      }
+// refactor logic later for simplicity
+const barChart = (xArray, yArray) => {
+  const chartId = document.querySelector("#bar-chart");
+  chartId.innerHTML = "";
+  const layout = { title: `A Project Manager's Task Load` };
+  const data = [
+    {
+      x: xArray,
+      y: yArray,
+      type: "bar",
+      orientation: "v",
+      marker: { color: "rgba(0,0,255)" },
+    },
+  ];
+  Plotly.newPlot(chartId, data, layout);
 };
 
+const pieChart = (xArray, yArray) => {
+  const chartId = document.querySelector("#pie-chart");
+  chartId.innerHTML = "";
+  const layout = { title: "Research Types" };
+  const data = [
+    {
+      labels: xArray,
+      values: yArray,
+      type: "pie",
+    },
+  ];
+  Plotly.newPlot(chartId, data, layout);
+};
 
+const getCharts = (event) => {
+  const chart_type = event.target.value;
+  try {
+    fetch(`/api/data?chart_type=${chart_type}`)
+      .then((res) => res.json())
+      .then((chartData) => {
+        document.querySelector("#bar-chart").style.display = "none";
+        document.querySelector("#pie-chart").style.display = "none";
+
+        if (chart_type == "pm_vs_category" && chartData.length > 0) {
+          let xArray = chartData.map((item) => item.project_manager);
+          let yArray = chartData.map((item) => item.count);
+          document.querySelector("#bar-chart").style.display = "block";
+          barChart(xArray, yArray);
+
+        } else if (chart_type == "rsch_category" && chartData.length > 0) {
+          let xArray = chartData.map((item) => item.research_type);
+          let yArray = chartData.map((item) => item.count);
+          document.querySelector("#pie-chart").style.display = "block";
+          pieChart(xArray, yArray);
+
+        } else {
+          console.log("nothing to see here");
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
